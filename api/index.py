c3a359f3e2258ee9abf042571608c817bb883f10
unknown <aphysics.nerd@gmail.com>
2025-05-25 18:25:57 +0330

    first commit

--- api/index.py
+++ api/index.py
@@ -1,11 +1,31 @@
 from flask import Flask
 
+
+messages_ = [
+    """Title: This just a test.
+---
+Hey this is just a test.
+this is a test and nithing else."""
+]
+
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/send')
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
+@app.route('/inbox<id>' )
+def home(id):
+    if id == 0:
+        for i in range(len(messages_)):
+            print(i+1,":", messages_[i].splitlines()[0])
+    global messages_
+    return messages_[id-1]
+
+
+@app.route('/status')
 def about():
-    return 'About'
+    global messages_
+
+    return len(messages_)
